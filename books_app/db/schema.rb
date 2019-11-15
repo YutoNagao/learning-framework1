98e93cd4524acc09e792eac4ac425b15b763f5d8
Yuto Nagao <yuwyuw1101@gmail.com>
2019-11-15 10:51:21 +0900

    11/15 課題

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_011512) do
+ActiveRecord::Schema.define(version: 2019_11_15_014134) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
+    t.string "picture"
   end
 
 end
